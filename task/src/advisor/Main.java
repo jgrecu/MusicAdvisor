4a4105f87c9ffc48bfca54247f10aa8d293396bf
Jeremy Grecu <jeremy.grecu@sap.com>
2021-11-04 09:43:41 +0000

    JBA - Music Advisor - Stage 1/5: Strings and fiddlesticks

--- task/src/advisor/Main.java
+++ task/src/advisor/Main.java
@@ -2,6 +2,8 @@ package advisor;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Menu menu = new Menu();
+
+        menu.start();
     }
 }
